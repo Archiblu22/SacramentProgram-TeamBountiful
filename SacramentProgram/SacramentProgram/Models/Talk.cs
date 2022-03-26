86b55ddbc6be43fbfa6ab08c46485911882bd8be
archiblue22 <paguirre@byui.edu>
2022-03-26 12:33:14 -0600

    added models

---
+++ SacramentProgram/SacramentProgram/Models/Talk.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SacramentProgram.Models
+{
+    public class Talk
+    {
+        public string Speaker { get; set; }
+        public string Subject { get; set; }
+    }
+}
